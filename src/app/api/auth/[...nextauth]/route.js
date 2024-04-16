fb8023ff23d56b04cedba9f53e6462e5feab9ca3
Aviral-Sharma-2578 <aviralshar10@gmail.com>
2024-04-16 21:06:20 +0530

    versions

--- src/app/api/auth/[...nextauth]/route.js
+++
@@ -1,5 +0,0 @@
-import NextAuth from "next-auth"
-import { options } from "./options"
-export const dynamic = "force-static";
- const handler = NextAuth(options);
- export { handler as GET , handler as POST}
